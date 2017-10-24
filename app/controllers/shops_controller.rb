54818e0269ab81fa18d8ff6771a50e0ffa91f97b
meldeeb <mohammad.edeeb@gmail.com>
2017-10-24 14:57:26 +0200

    Add pagination to shops and categories

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,7 +4,7 @@ class ShopsController < ApplicationController
   # GET /shops
   # GET /shops.json
   def index
-    @shops = Shop.all
+    @shops = Shop.paginate(:page => params[:page], :per_page => 10).order("name ASC")
   end
 
   # GET /shops/1
