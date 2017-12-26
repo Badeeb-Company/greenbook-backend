af198fae762a47dc80e305baa2b81f46d18d4aaf
meldeeb <mohammad.edeeb@gmail.com>
2017-12-27 01:52:15 +0200

    Fix shop photo url

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -18,8 +18,13 @@ class Shop < ApplicationRecord
 	acts_as_mappable :lat_column_name => :lat, :lng_column_name => :long
 
 	def self.initialize_from_place(place)
+		main_photo_url = place.icon
+		if(place.photos[0])
+			main_photo_url = place.photos[0].fetch_url(400)
+		end
 		shop = Shop.new(lat: place.lat, long: place.lng, google_place_id: place.place_id,
-			name: place.name, address: place.vicinity, main_photo_url: place.icon)
+			name: place.name, address: place.vicinity,
+			main_photo_url: main_photo_url)
 	end
 
 	def self.new_with_working_days
